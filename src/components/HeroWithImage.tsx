d22a5ed1b3aacc6ae855c8a1968f07b5adcd5cc4
bartdoping <bartdoping@gmail.com>
2025-10-20 18:04:13 +0200

    feat(home-hero): add responsive top padding above main headline for better visual spacing

--- src/components/HeroWithImage.tsx
+++ src/components/HeroWithImage.tsx
@@ -22,7 +22,7 @@ export default function HeroWithImage() {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 modern-container text-center text-white">
+      <div className="relative z-10 modern-container text-center text-white pt-12 sm:pt-16 md:pt-20">
         
         
         {/* Main Headline */}
